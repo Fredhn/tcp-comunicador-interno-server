c077df6434a7758311c9c260d47038e3078243bd
Fred Neves <fred_div@hotmail.com>
2017-04-27 23:53:09 -0300

    Add files via upload

---
+++ ToolServer.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace CommunicationTool_Server
+{
+    partial class ToolServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToolServer));
+            this.pnl_Main = new System.Windows.Forms.Panel();
+            this.panel_ButtonsServer = new System.Windows.Forms.Panel();
+            this.pictureBox_BroadcastMsg = new System.Windows.Forms.PictureBox();
+            this.btn_startServer = new System.Windows.Forms.Button();
+            this.richTextBox_chatInput = new System.Windows.Forms.RichTextBox();
+            this.richTextBox_chatStream = new System.Windows.Forms.RichTextBox();
+            this.pnl_Main.SuspendLayout();
+            this.panel_ButtonsServer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_BroadcastMsg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_Main
+            // 
+            this.pnl_Main.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pnl_Main.Controls.Add(this.panel_ButtonsServer);
+            this.pnl_Main.Controls.Add(this.richTextBox_chatInput);
+            this.pnl_Main.Controls.Add(this.richTextBox_chatStream);
+            this.pnl_Main.Location = new System.Drawing.Point(0, -2);
+            this.pnl_Main.Name = "pnl_Main";
+            this.pnl_Main.Size = new System.Drawing.Size(483, 306);
+            this.pnl_Main.TabIndex = 1;
+            // 
+            // panel_ButtonsServer
+            // 
+            this.panel_ButtonsServer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel_ButtonsServer.Controls.Add(this.pictureBox_BroadcastMsg);
+            this.panel_ButtonsServer.Controls.Add(this.btn_startServer);
+            this.panel_ButtonsServer.Location = new System.Drawing.Point(4, 194);
+            this.panel_ButtonsServer.Name = "panel_ButtonsServer";
+            this.panel_ButtonsServer.Size = new System.Drawing.Size(474, 39);
+            this.panel_ButtonsServer.TabIndex = 2;
+            // 
+            // pictureBox_BroadcastMsg
+            // 
+            this.pictureBox_BroadcastMsg.Image = global::CommunicationTool_Server.Properties.Resources.red_mail_send_icon;
+            this.pictureBox_BroadcastMsg.Location = new System.Drawing.Point(420, -2);
+            this.pictureBox_BroadcastMsg.Name = "pictureBox_BroadcastMsg";
+            this.pictureBox_BroadcastMsg.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox_BroadcastMsg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox_BroadcastMsg.TabIndex = 1;
+            this.pictureBox_BroadcastMsg.TabStop = false;
+            this.pictureBox_BroadcastMsg.Click += new System.EventHandler(this.pictureBox_BroadcastMsg_Click);
+            // 
+            // btn_startServer
+            // 
+            this.btn_startServer.BackColor = System.Drawing.Color.LightGray;
+            this.btn_startServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_startServer.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_startServer.Location = new System.Drawing.Point(7, 1);
+            this.btn_startServer.Name = "btn_startServer";
+            this.btn_startServer.Size = new System.Drawing.Size(116, 30);
+            this.btn_startServer.TabIndex = 0;
+            this.btn_startServer.Text = "Iniciar Servidor";
+            this.btn_startServer.UseVisualStyleBackColor = false;
+            this.btn_startServer.Click += new System.EventHandler(this.btn_startServer_Click);
+            // 
+            // richTextBox_chatInput
+            // 
+            this.richTextBox_chatInput.Location = new System.Drawing.Point(4, 232);
+            this.richTextBox_chatInput.Name = "richTextBox_chatInput";
+            this.richTextBox_chatInput.Size = new System.Drawing.Size(474, 62);
+            this.richTextBox_chatInput.TabIndex = 1;
+            this.richTextBox_chatInput.Text = "";
+            this.richTextBox_chatInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.CheckEnterKeyPress);
+            // 
+            // richTextBox_chatStream
+            // 
+            this.richTextBox_chatStream.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox_chatStream.Enabled = false;
+            this.richTextBox_chatStream.Location = new System.Drawing.Point(4, 4);
+            this.richTextBox_chatStream.Name = "richTextBox_chatStream";
+            this.richTextBox_chatStream.Size = new System.Drawing.Size(474, 189);
+            this.richTextBox_chatStream.TabIndex = 0;
+            this.richTextBox_chatStream.Text = "";
+            this.richTextBox_chatStream.TextChanged += new System.EventHandler(this.richTextBox_TextChanged);
+            // 
+            // ToolServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 302);
+            this.Controls.Add(this.pnl_Main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ToolServer";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Communication Tool - Server";
+            this.pnl_Main.ResumeLayout(false);
+            this.panel_ButtonsServer.ResumeLayout(false);
+            this.panel_ButtonsServer.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_BroadcastMsg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox_chatStream;
+        private System.Windows.Forms.RichTextBox richTextBox_chatInput;
+        private System.Windows.Forms.Button btn_startServer;
+        private System.Windows.Forms.PictureBox pictureBox_BroadcastMsg;
+        private System.Windows.Forms.Panel panel_ButtonsServer;
+        private System.Windows.Forms.Panel pnl_Main;
+    }
+}
+
---
+++ ToolServer.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommunicationTool_Server
+{
+    public partial class ToolServer : Form
+    {
+        private bool serverStatus = false;
+        private byte[] _buffer = new byte[1024];
+        public List<SocketT2h> __ClientSockets { get; set; }
+        List<string> _names = new List<string>();
+        private Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public ToolServer()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            __ClientSockets = new List<SocketT2h>();
+        }
+
+        private void btn_startServer_Click(object sender, EventArgs e)
+        {
+            ControlServerStatus();
+        }
+
+        private void ControlServerStatus()
+        {
+            if (serverStatus == false)
+            {
+                serverStatus = true;
+                SetupServer();
+                btn_startServer.Enabled = false;
+                btn_startServer.Text = "Servidor ON";
+                btn_startServer.BackColor = System.Drawing.Color.Red;
+            }
+        }
+        private void SetupServer()
+        {
+            richTextBox_chatStream.Text += "Iniciando servidor . . . \n";
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Any, 100));
+            _serverSocket.Listen(1);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private void AppceptCallback(IAsyncResult ar)
+        {
+            Socket socket = _serverSocket.EndAccept(ar);
+            __ClientSockets.Add(new SocketT2h(socket));
+            //list_Client.Items.Add(socket.RemoteEndPoint.ToString());
+
+            //lb_soluong.Text = "Số client đang kết nối: " + __ClientSockets.Count.ToString();
+            richTextBox_chatStream.Text += "Client connected. . . \n";
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+
+            Socket socket = (Socket)ar.AsyncState;
+            if (socket.Connected)
+            {
+                int received;
+                try
+                {
+                    received = socket.EndReceive(ar);
+                }
+                catch (Exception)
+                {
+                    // client đóng kết nối
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            __ClientSockets.RemoveAt(i);
+                            //lb_soluong.Text = "Số client đang kết nối: " + __ClientSockets.Count.ToString();
+                        }
+                    }
+                    // xóa trong list
+                    return;
+                }
+                if (received != 0)
+                {
+                    byte[] dataBuf = new byte[received];
+                    Array.Copy(_buffer, dataBuf, received);
+                    string text = Encoding.ASCII.GetString(dataBuf);
+                    //lb_stt.Text = "Text received: " + text;
+
+                    string reponse = string.Empty;
+                    //if (text.Contains("@@"))
+                    //{
+                    //    for (int i = 0; i < list_Client.Items.Count; i++)
+                    //    {
+                    //        if (socket.RemoteEndPoint.ToString().Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
+                    //        {
+                    //            list_Client.Items.RemoveAt(i);
+                    //            list_Client.Items.Insert(i, text.Substring(1, text.Length - 1));
+                    //            __ClientSockets[i]._Name = text;
+                    //            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+                    //            return;
+                    //        }
+                    //    }
+                    //}
+
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (socket.RemoteEndPoint.ToString().Equals(__ClientSockets[i]._Socket.RemoteEndPoint.ToString()))
+                        {
+                            richTextBox_chatStream.AppendText(__ClientSockets[i]._Name + ": " + text + "\n");
+                        }
+                    }
+
+                    for (int j = 0; j < __ClientSockets.Count; j++)
+                    {
+                        if (!(socket.RemoteEndPoint.ToString().Equals(__ClientSockets[j]._Socket.RemoteEndPoint.ToString())))
+                        {
+                            Sendata(__ClientSockets[j]._Socket, __ClientSockets[j]._Name + ": " + text + "\n");
+                        }
+                    }
+
+
+                    if (text == "bye \n")
+                    {
+                        return;
+                    }
+                    reponse = ": " + text + "\n";
+                    Sendata(socket, reponse);
+                }
+                else
+                {
+                    for (int i = 0; i < __ClientSockets.Count; i++)
+                    {
+                        if (__ClientSockets[i]._Socket.RemoteEndPoint.ToString().Equals(socket.RemoteEndPoint.ToString()))
+                        {
+                            __ClientSockets.RemoveAt(i);
+                            //lb_soluong.Text = "Số client đang kết nối: " + __ClientSockets.Count.ToString();
+                        }
+                    }
+                }
+            }
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+        }
+        void Sendata(Socket socket, string noidung)
+        {
+            byte[] data = Encoding.ASCII.GetBytes(noidung);
+            socket.BeginSend(data, 0, data.Length, SocketFlags.None, new AsyncCallback(SendCallback), socket);
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+        private void SendCallback(IAsyncResult AR)
+        {
+            Socket socket = (Socket)AR.AsyncState;
+            socket.EndSend(AR);
+        }
+
+        //private void btnSend_Click(object sender, EventArgs e)
+        //{
+        //    for (int j = 0; j < __ClientSockets.Count; j++)
+        //    {
+        //        //if (__ClientSockets[j]._Socket.Connected && __ClientSockets[j]._Name.Equals("@"+t))
+        //        {
+        //            Sendata(__ClientSockets[j]._Socket, richTextBox_chatInput.Text);
+        //        }
+        //    }
+        //    richTextBox_chatStream.AppendText("Server: " + richTextBox_chatInput.Text + "\n");
+        //}
+        private void pictureBox_BroadcastMsg_Click(object sender, EventArgs e)
+        {
+            if (richTextBox_chatInput.Text != "")
+            {
+                for (int j = 0; j < __ClientSockets.Count; j++)
+                {
+                    //if (__ClientSockets[j]._Socket.Connected && __ClientSockets[j]._Name.Equals("@"+t))
+                    {
+                        Sendata(__ClientSockets[j]._Socket, ": Server diz: " + richTextBox_chatInput.Text + "\n");
+                    }
+                }
+                richTextBox_chatStream.AppendText(": Server diz: " + richTextBox_chatInput.Text + "\n");
+                richTextBox_chatInput.Text = "";
+            }
+        }
+        private void CheckEnterKeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+
+            {
+                if (richTextBox_chatInput.Text != "")
+                {
+                    for (int j = 0; j < __ClientSockets.Count; j++)
+                    {
+                        //if (__ClientSockets[j]._Socket.Connected && __ClientSockets[j]._Name.Equals("@"+t))
+                        {
+                            Sendata(__ClientSockets[j]._Socket, ": Server diz: " + richTextBox_chatInput.Text + "\n");
+                        }
+                    }
+                    richTextBox_chatStream.AppendText(": Server diz: " + richTextBox_chatInput.Text + "\n");
+                    richTextBox_chatInput.Text = "";
+                }
+            }
+        }
+
+        private void richTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // set the current caret position to the end
+            richTextBox_chatStream.SelectionStart = richTextBox_chatStream.Text.Length;
+            // scroll it automatically
+            richTextBox_chatStream.ScrollToCaret();
+        }
+    }
+    public class SocketT2h
+    {
+        public Socket _Socket { get; set; }
+        public string _Name { get; set; }
+        public SocketT2h(Socket socket)
+        {
+            this._Socket = socket;
+        }
+    }
+}
